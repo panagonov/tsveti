f34cc9d83ec9ed97c3648261d275c68792d6e364
panagonov <panagonov@gmail.com>
2018-06-30 21:53:05 +0300

    t

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-# - *- coding: cp1252 -*-
+# -*- coding: utf-8 -*import
 
-for x in range (1, 10) :
-	print(x)
+a = 2; b = 3; c = 4; x = a + b * c;
+print(x)
+x = 0
+while x < 5 :
+    print(x)
+    x += 1
+print("cat")
+for x in range (1 , 10) : print(x)
